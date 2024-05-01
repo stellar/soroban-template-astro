063fcf4c9a1b6b6b0d7e111dec09443ccc9a4c76
Elliot Voris <elliot@voris.me>
2024-05-01 16:01:47 -0500

    Fix the import content to work with the new SDK contract client (#2)
    * fix the import content to work with the new SDK contract client
    
    * add the endpoint URL path for local rpc
    
    * Update src/contracts/util.ts
    
    Co-authored-by: Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>

--- src/contracts/util.ts
+++ src/contracts/util.ts
@@ -1,2 +1,2 @@
-export const rpcUrl = import.meta.env.PUBLIC_SOROBAN_RPC_URL ?? "http://localhost:8000/"
+export const rpcUrl = import.meta.env.PUBLIC_SOROBAN_RPC_URL ?? "http://localhost:8000/rpc"
 export const networkPassphrase = import.meta.env.PUBLIC_SOROBAN_NETWORK_PASSPHRASE ?? "Standalone Network ; February 2017"
