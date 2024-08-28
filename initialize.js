f99af17c7776b0a8e2175b3fed03717123a0a889
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2024-08-28 13:54:45 -0400

    feat: clean & add comments to initialize.js
    From looking at
    https://developers.stellar.org/docs/build/guides/dapps/soroban-contract-init-template,
    my guess is that people do not find the existing `initialize.js` script
    easy to understand.
    
    I've followed the lead of @CaptainPrinz here and changed the name of
    `exe` to `run`, and then I added lots of comments to each function.
    
    I also removed the `SOROBAN_NETWORK` variable, as I think it makes this
    somewhat more confusing instead of more clear. This means removing the
    `network.${process.env.SOROBAN_NETWORK}` shortcut, but maybe that's ok?
    
    I also moved around things in the `.gitignore`, just so all the
    specific-to-this-template stuff would be at the bottom. A new
    frontend-template guide I am writing is going to mention and link to
    this stuff.

--- initialize.js
+++ initialize.js
@@ -1,35 +1,38 @@
-import 'dotenv/config';
-import { mkdirSync, writeFileSync, rmSync, readFileSync } from 'fs';
-import { execSync } from 'child_process';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { sync as glob } from 'glob';
+import "dotenv/config";
+import { mkdirSync, writeFileSync, rmSync, readFileSync } from "fs";
+import { execSync } from "child_process";
+import path from "path";
+import { fileURLToPath } from "url";
+import { sync as glob } from "glob";
 
 // Load environment variables starting with PUBLIC_ into the environment,
 // so we don't need to specify duplicate variables in .env
 for (const key in process.env) {
-  if (key.startsWith('PUBLIC_')) {
+  if (key.startsWith("PUBLIC_")) {
     process.env[key.substring(7)] = process.env[key];
   }
 }
 
-console.log('###################### Initializing ########################');
+console.log("###################### Initializing ########################");
 
-// Get dirname (equivalent to the Bash version)
+// Get dirname (equivalent to __dirname in CommonJS modules)
 const __filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(__filename);
 
-// variable for later setting pinned version of soroban in "$(dirname/target/bin/soroban)"
-const cli = 'stellar';
-
-// Function to execute and log shell commands
-function exe(command) {
+/**
+ * Execute and log a shell command
+ */
+function run(command) {
   console.log(command);
-  execSync(command, { stdio: 'inherit' });
+  execSync(command, { stdio: "inherit" });
 }
 
-function fundAll() {
-  exe(`${cli} keys generate ${process.env.SOROBAN_ACCOUNT}`);
+/**
+ * Generate a new keypair for the account specified by `SOROBAN_ACCOUNT` in the
+ * `.env` file (loaded into the environment by `dotenv`)
+ */
+function generateAccount() {
+  run(`stellar keys generate ${process.env.SOROBAN_ACCOUNT}`);
 }
 
 function removeFiles(pattern) {
@@ -37,50 +40,96 @@ function removeFiles(pattern) {
   glob(pattern).forEach((entry) => rmSync(entry));
 }
 
+/**
+ * Build all contracts, removing outdated `.wasm` files so stale artifacts
+ * don't accidentally get deployed, bound, and imported later
+ */
 function buildAll() {
   removeFiles(`${dirname}/target/wasm32-unknown-unknown/release/*.wasm`);
   removeFiles(`${dirname}/target/wasm32-unknown-unknown/release/*.d`);
-  exe(`${cli} contract build`);
+  run(`stellar contract build`);
 }
 
+/**
+ * Get the filename without the extension
+ */
 function filenameNoExtension(filename) {
   return path.basename(filename, path.extname(filename));
 }
 
+/**
+ * Deploy a contract, given its `.wasm` file, creating an alias for it based on
+ * the filename. The alias will create a `.soroban/contract-ids` directory,
+ * which is assumed to be present later.
+ */
 function deploy(wasm) {
-  exe(`${cli} contract deploy --wasm ${wasm} --ignore-checks --alias ${filenameNoExtension(wasm)}`);
+  run(
+    `stellar contract deploy --wasm ${wasm} --ignore-checks --alias ${filenameNoExtension(
+      wasm
+    )}`
+  );
 }
 
+/**
+ * Deploy all contracts in the `target/wasm32-unknown-unknown/release`
+ * directory. These need to be built first, so this function should be called
+ * after `buildAll`.
+ */
 function deployAll() {
-  const contractsDir = `${dirname}/.soroban/contract-ids`;
-  mkdirSync(contractsDir, { recursive: true });
-
-  const wasmFiles = glob(`${dirname}/target/wasm32-unknown-unknown/release/*.wasm`);
+  const wasmFiles = glob(
+    `${dirname}/target/wasm32-unknown-unknown/release/*.wasm`
+  );
 
   wasmFiles.forEach(deploy);
 }
 
+/**
+ * Get contract aliases and IDs from the `.soroban/contract-ids` directory,
+ * using the filename as the alias. Filters out contracts that weren't deployed
+ * to the network specified in the `.env` file.
+ */
 function contracts() {
   const contractFiles = glob(`${dirname}/.soroban/contract-ids/*.json`);
 
   return contractFiles
-    .map(path => ({
+    .map((path) => ({
       alias: filenameNoExtension(path),
-      ...JSON.parse(readFileSync(path))
+      ...JSON.parse(readFileSync(path)),
     }))
-    .filter(data => data.ids[process.env.SOROBAN_NETWORK_PASSPHRASE])
-    .map(data => ({alias: data.alias, id: data.ids[process.env.SOROBAN_NETWORK_PASSPHRASE]}));
+    .filter((data) => data.ids[process.env.SOROBAN_NETWORK_PASSPHRASE])
+    .map((data) => ({
+      alias: data.alias,
+      id: data.ids[process.env.SOROBAN_NETWORK_PASSPHRASE],
+    }));
 }
 
-function bind({alias, id}) {
-  exe(`${cli} contract bindings typescript --contract-id ${id} --output-dir ${dirname}/packages/${alias} --overwrite`);
+/**
+ * Generate TypeScript bindings for a contract, given its alias and ID. The
+ * bindings will be output to a directory named after the alias in the
+ * `packages` directory. Everything in `packages` is set up as part of the NPM
+ * workspace in `package.json`.
+ */
+function bind({ alias, id }) {
+  run(
+    `stellar contract bindings typescript --contract-id ${id} --output-dir ${dirname}/packages/${alias} --overwrite`
+  );
 }
 
+/**
+ * Generate TypeScript bindings for all contracts
+ */
 function bindAll() {
   contracts().forEach(bind);
 }
 
-function importContract({id, alias}) {
+/**
+ * Create a file in the project's `src/contracts` directory that imports the
+ * contract client from a package generated with `soroban contract bindings
+ * typescript`, as in `bindAll` above. Then instantiate the client with the
+ * environment settings from `.env`. This file will be used to import the
+ * contract client in the rest of the project.
+ */
+function importContract({ id, alias }) {
   const outputDir = `${dirname}/src/contracts/`;
 
   mkdirSync(outputDir, { recursive: true });
@@ -89,10 +138,12 @@ function importContract({id, alias}) {
     `import * as Client from '${alias}';\n` +
     `import { rpcUrl } from './util';\n\n` +
     `export default new Client.Client({\n` +
-    `  ...Client.networks.${process.env.SOROBAN_NETWORK},\n` +
+    `  contractId: '${id}',\n` +
+    `  networkPassphrase: '${process.env.SOROBAN_NETWORK_PASSPHRASE}',\n` +
     `  rpcUrl,\n` +
     `${
-      process.env.SOROBAN_NETWORK === "local" || "standalone"
+      process.env.SOROBAN_NETWORK_PASSPHRASE ===
+      "Standalone Network ; February 2017"
         ? `  allowHttp: true,\n`
         : null
     }` +
@@ -105,12 +156,15 @@ function importContract({id, alias}) {
   console.log(`Created import for ${alias}`);
 }
 
+/**
+ * Create import files in `src/contracts` for all contracts
+ */
 function importAll() {
   contracts().forEach(importContract);
 }
 
-// Calling the functions (equivalent to the last part of your bash script)
-fundAll();
+// Main part of script. Run all functions.
+generateAccount();
 buildAll();
 deployAll();
 bindAll();
