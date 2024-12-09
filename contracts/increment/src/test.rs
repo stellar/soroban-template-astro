20e4435b12db62b9e79fe81c5796e2351f7e60e1
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2024-12-09 13:54:12 -0500

    feat: add example contracts
    The `stellar contract init` command no longer works with
    `--frontend-template` (https://github.com/stellar/stellar-cli/pull/1625)
    
    This means that templates like this one need to hard-code their own list
    of contracts.

---
+++ contracts/increment/src/test.rs
@@ -0,0 +1,14 @@
+#![cfg(test)]
+use crate::{IncrementContract, IncrementContractClient};
+use soroban_sdk::Env;
+
+#[test]
+fn test() {
+    let env = Env::default();
+    let contract_id = env.register(IncrementContract, ());
+    let client = IncrementContractClient::new(&env, &contract_id);
+
+    assert_eq!(client.increment(), 1);
+    assert_eq!(client.increment(), 2);
+    assert_eq!(client.increment(), 3);
+}
